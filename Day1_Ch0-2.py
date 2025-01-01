38e593d002a51849958960eadc118e174b6097ec
FrankCao000 <frankyiyuan089@gmail.com>
2025-01-01 17:42:33 -0500

    update

--- Day1_Ch0-2.py
+++ Day1_Ch0-2.py
@@ -65,6 +65,7 @@ def algo_C(n : int):
     for _ in range(10000):
         print(0)
 
+##Time-Complexity
 ##O(1)
 def constant(n: int) -> int:
     count = 0
@@ -105,3 +106,101 @@ def bubble_sort(nums: list[int]) -> int:
                 nums[j+1] = tmp
                 count += 3
     return count
+
+##O(2^n)
+def exponnential(n: int) -> int:
+    count = 0
+    base = 1
+    for _ in range(n):
+        for _ in range(base):
+            count += 1
+        base += 2
+    # count = 1 + 2 + 4 + 8 + ... + 2^(n-1) = 2^n - 1
+    return count
+
+def exp_recur(n: int) -> int:
+    if n == 1:
+        return 1
+    return exp_recur(n-1) + exp_recur(n-1) + 1
+
+## Olog(n)
+def logarithmic(n: int) -> int:
+    count = 0
+    while n > 1:
+        n = n/2
+        count += 1
+    return count
+
+def log_recur(n: int) -> int:
+    if n <= 1:
+        return 0
+    return log_recur(n/2) + 1
+
+## O(nlog(n))
+def linear_log_recur(n: int) -> int:
+    if n <= 1:
+        return 1
+    count = linear_log_recur(n//2) + linear_log_recur(n//2)
+    for _ in range(n):
+        count += 1
+    return count
+
+## O(n!)
+def factorial_recur(n: int) -> int:
+    if n == 0:
+        return 1
+    count = 0
+    for _ in range(n):
+        count += factorial_recur(n-1)
+    return count
+
+## Best/Worst time complexity
+import random
+def random_numbers(n: int) -> list[int]:
+    nums = [i for i in range(1, n + 1)]
+    random.shuffle(nums)
+    return nums
+
+def find_one(nums: list[int]) -> int:
+    for i in range(len(nums)):
+    ## when 1 is in the front: best time complexity O(1)
+    ## when i is in the end: worst time complexity O(n)
+        if nums[i] == 1:
+            return i
+    return -1
+
+##Space-Complexity
+## O(1)
+def function() -> int:
+    return 0
+
+from modules import ListNode
+def constant(n: int):
+    a = 0
+    nums = [0] * 1000
+    node = ListNode(0)
+    ## variable in loop takes space_complexity of O(1)
+    for _ in range(n):
+        c = 0
+    ## function in loop takes space_complexity of O(1)
+    for _ in range(n):
+        function()
+
+## O(n)
+def linear(n: int):
+    nums = [0] * n
+    hmap = dict[int, str]()
+    for i in range(n):
+        hmap[i] = str(i)
+
+def linear_recur(n: int):
+    print("Recursed n = ", n)
+    if n == 1:
+        return
+    linear_recur(n - 1)
+
+## O(n^2)
+def quadratic(n: int):
+    num_matrix = [[0] * n for _ in range(n)]
+
+
